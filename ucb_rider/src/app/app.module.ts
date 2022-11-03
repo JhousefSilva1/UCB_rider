8f5d4814d044ad8c73ece49e22940506e5bd3965
Jose Pozo Silva <jhosede23@gmail.com>
2022-11-03 00:52:41 -0400

    updating

--- ucb_rider/src/app/app.module.ts
+++ ucb_rider/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { IndexComponent } from './components/index/index.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MotorcycleComponent } from './components/motorcycle/motorcycle.component';
+import { QuotationComponent } from './components/quotation/quotation.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    IndexComponent,
+    FooterComponent,
+    MotorcycleComponent,
+    QuotationComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
